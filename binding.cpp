0b069e3a953990f49fcb020c424823cde309f8aa
Dane Springmeyer <dane@mapbox.com>
2014-11-17 22:28:34 -0800

    test putenv/setenv

--- binding.cpp
+++ binding.cpp
@@ -33,11 +33,43 @@ static NAN_METHOD(addon_getenv)
     }
 }
 
+static NAN_METHOD(addon_setenv)
+{
+    NanScope();
+    if (args.Length() == 2) {
+        if (!args[0]->IsString()) {
+            NanThrowTypeError("first argument must be a String");
+            NanReturnUndefined();
+        }
+        if (!args[1]->IsString()) {
+            NanThrowTypeError("second argument must be a String");
+            NanReturnUndefined();
+        }
+        std::string key = *v8::String::Utf8Value(args[0]->ToString());
+        std::string value = *v8::String::Utf8Value(args[1]->ToString());
+        if (!key.empty() && !value.empty()) {
+#if defined(_MSC_VER)
+            int ret = _putenv_s(key.c_str(), value.c_str());
+#else
+            int ret = setenv(key.c_str(), value.c_str(), 1);
+#endif
+            NanReturnValue(NanNew(ret));
+        } else {
+            NanThrowTypeError("expects non empty key, value");
+            NanReturnUndefined();
+        }
+    } else {
+        NanThrowTypeError("expects two arguments: a key and value");
+        NanReturnUndefined();
+    }
+    NanReturnUndefined();
+}
 
 extern "C" {
     static void start(v8::Handle<v8::Object> target) {
         NanScope();
         NODE_SET_METHOD(target, "getenv", addon_getenv);
+        NODE_SET_METHOD(target, "setenv", addon_setenv);
     }
 }
 
--- test.js
+++ test.js
@@ -5,9 +5,9 @@ var binding;
 
 tape('process.env can set and get variables', function(assert) {
     // env variable set before addon is required
-    process.env.ENV_VARIABLE_A = "before";
+    process.env.VARIABLE_1 = "VARIABLE_1";
     // make sure node.js thinks it is set
-    assert.equal(process.env.ENV_VARIABLE_A,"before");
+    assert.equal(process.env.VARIABLE_1,"VARIABLE_1");
     assert.end();
 });
 
@@ -15,16 +15,27 @@ tape('getenv sees process.env variable set before addon is required', function(a
     // require addon now
     binding = require('./');
     // should be equal
-    assert.equal(process.env.ENV_VARIABLE_A,binding.getenv('ENV_VARIABLE_A'));
+    assert.equal(process.env.VARIABLE_2,binding.getenv('VARIABLE_2'));
     assert.end();
 });
 
 tape('getenv sees process.env variable set after addon is required', function(assert) {
     // create a second env variable
-    process.env.ENV_VARIABLE_B = "after";
+    process.env.VARIABLE_2 = "VARIABLE_2";
     // make sure node.js thinks it is set
-    assert.equal(process.env.ENV_VARIABLE_B,"after");
+    assert.equal(process.env.VARIABLE_2,"VARIABLE_2");
     // should be equal
-    assert.equal(process.env.ENV_VARIABLE_B,binding.getenv('ENV_VARIABLE_B'));
+    assert.equal(process.env.VARIABLE_2,binding.getenv('VARIABLE_2'));
+    assert.end();
+});
+
+tape('getenv sees variable set by setenv/putenv', function(assert) {
+    assert.equal(0,binding.setenv("VARIABLE_3","VARIABLE_3"));
+    // create a second env variable
+    process.env.VARIABLE_3 = "VARIABLE_3";
+    // make sure node.js thinks it is set
+    assert.equal(process.env.VARIABLE_3,"VARIABLE_3");
+    // should be equal
+    assert.equal(process.env.VARIABLE_3,binding.getenv('VARIABLE_3'));
     assert.end();
 });
