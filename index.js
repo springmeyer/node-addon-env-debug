7c0bee786a5223e8da7756203927021df44349e0
Dane Springmeyer <dane@mapbox.com>
2014-11-17 19:52:20 -0800

    test env variables before and after requiring module

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var binding = require('./build/Release/node-addon-env-debug.node')
 var assert = require('assert');
 
-process.env.ICU_DATA = "this";
-var value = binding.getenv('ICU_DATA');
-assert.equal(value,"this");
+// env variable set before addon is required
+process.env.ENV_VARIABLE_A = "before";
+
+// make sure node.js thinks it is set
+assert.equal(process.env.ENV_VARIABLE_A,"before");
+
+// require addon now
+var binding = require('./build/Release/node-addon-env-debug.node')
+
+// should be equal
+assert.equal(process.env.ENV_VARIABLE_A,binding.getenv('ENV_VARIABLE_A'));
+
+// create a second env variable
+process.env.ENV_VARIABLE_B = "after";
+
+// make sure node.js thinks it is set
+assert.equal(process.env.ENV_VARIABLE_B,"after");
+
+// should be equal
+assert.equal(process.env.ENV_VARIABLE_B,binding.getenv('ENV_VARIABLE_B'));
