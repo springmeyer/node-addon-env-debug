f99737bef10011dfbc88a34bbe50dbdef83b45c2
Dane Springmeyer <dane@mapbox.com>
2014-11-17 22:14:52 -0800

    convert tests to tape

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
-var assert = require('assert');
+var tape = require('tape');
+var path = require('path');
 
-// env variable set before addon is required
-process.env.ENV_VARIABLE_A = "before";
 
-// make sure node.js thinks it is set
-assert.equal(process.env.ENV_VARIABLE_A,"before");
+tape('process.env can set and get variables', function(assert) {
+    // env variable set before addon is required
+    process.env.ENV_VARIABLE_A = "before";
+    // make sure node.js thinks it is set
+    assert.equal(process.env.ENV_VARIABLE_A,"before");
+    assert.end();
+});
 
-// require addon now
-var binding = require('./');
-
-// should be equal
-assert.equal(process.env.ENV_VARIABLE_A,binding.getenv('ENV_VARIABLE_A'));
-
-// create a second env variable
-process.env.ENV_VARIABLE_B = "after";
-
-// make sure node.js thinks it is set
-assert.equal(process.env.ENV_VARIABLE_B,"after");
-
-// should be equal
-assert.equal(process.env.ENV_VARIABLE_B,binding.getenv('ENV_VARIABLE_B'));
+tape('process.env can set and get variables', function(assert) {
+    // require addon now
+    var binding = require('./');
+    // should be equal
+    assert.equal(process.env.ENV_VARIABLE_A,binding.getenv('ENV_VARIABLE_A'));
+    // create a second env variable
+    process.env.ENV_VARIABLE_B = "after";
+    // make sure node.js thinks it is set
+    assert.equal(process.env.ENV_VARIABLE_B,"after");
+    // should be equal
+    assert.equal(process.env.ENV_VARIABLE_B,binding.getenv('ENV_VARIABLE_B'));
+    assert.end();
+});
 
 console.log('all tests pass')
